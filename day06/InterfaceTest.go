8bc349a671a4ab4c9fdb14bf70d0d94d3f499dd7
melooo <1092106874@qq.com>
2020-11-19 08:49:06 +0800

    day06 the second commit

---
+++ day06/InterfaceTest.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+type Animal struct{
+
+}
+
+type behavior interface {
+	eat()
+	run()
+}
+
+type Cat struct{
+	Animal
+}
+
+type Dog struct{
+	Animal
+}
+
+func (cat Cat) eat(){
+	fmt.Println("我是cat,我会eat~")
+}
+
+func (dog Dog) eat(){
+	fmt.Println("我是dog,我会eat~")
+}
+
+func (cat Cat) run(){
+	fmt.Println("我是cat,我会run~")
+}
+
+func (dog Dog) run(){
+	fmt.Println("我是dog,我会run~")
+}
+
+func main(){
+
+	var temp behavior
+	var cat Cat
+	temp=cat
+	temp.run()
+	temp.eat()
+	var dog Dog
+	temp=dog
+	temp.run()
+	temp.eat()
+}
